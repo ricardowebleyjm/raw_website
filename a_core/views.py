80e1c8a9f6616d9ccfc1a759b88dc3545586a1c8
Ricardo Webley <Rwebley@duck.com>
2026-04-12 18:14:43 -0500

    Page Caching

--- a_core/views.py
+++ a_core/views.py
@@ -3,11 +3,16 @@ from django.http import HttpRequest
 from django.contrib import messages
 from .models import Project
 from .forms import ContactForm
+from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 
+@cache_page(60 * 5) 
 def home(request: HttpRequest):
     featured_projects = Project.objects.filter(is_featured=True)[:3]
     return render(request, 'a_core/home.html', {'projects': featured_projects})
 
+@vary_on_headers("Cookie")
+@cache_page(60 * 2)
 def contact(request: HttpRequest):
     if request.method == 'POST':
         form = ContactForm(request.POST)
@@ -22,9 +27,11 @@ def contact(request: HttpRequest):
         
     return render(request, 'a_core/contact.html', {'form': form})
 
+@cache_page(60 * 5) 
 def projects(request: HttpRequest):
     all_projects = Project.objects.prefetch_related('skills').all()
     return render(request, 'a_core/projects.html', {'projects': all_projects})
 
+@cache_page(60 * 5) 
 def portfolio(request: HttpRequest):
     return redirect('projects')
